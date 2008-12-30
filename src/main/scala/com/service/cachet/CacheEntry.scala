9779cb8a41bdcd71fa39e645c18a76b1e8ffc0a5
pivotal <pivotal@Toemang.local>
2008-12-30 11:14:22 -0800

    cleanup

--- src/main/scala/com/service/cachet/CacheEntry.scala
+++ src/main/scala/com/service/cachet/CacheEntry.scala
@@ -5,32 +5,24 @@ import scala.util.matching.Regex
 class CacheEntry(response: ResponseWrapper) {
   val requestTime = System.currentTimeMillis
   var responseTime = 0.toLong
-  
+
   def noteResponseTime() {
     responseTime = System.currentTimeMillis
   }
-  
+
   def dateValue = response getDateHeader("Date") getOrElse responseTime
-
   def apparentAge = (responseTime - dateValue) max 0
-
   def ageValue = response getIntHeader("Age") map (_.toLong)
-
   def correctedReceivedAge = ageValue map (_ max apparentAge) getOrElse apparentAge
-
   def responseDelay = responseTime - requestTime
-
   def correctedInitialAge = correctedReceivedAge + responseDelay
-
   def residentTime = System.currentTimeMillis - responseTime
-
   def currentAge = correctedInitialAge + residentTime
-
   def expiresValue = response getDateHeader("Expires")
-  
+
   private val MaxAge = """\b(?:s-maxage|max-age)=(\d+)\b""".r
 
-  def maxAgeValue = { 
+  def maxAgeValue = {
     for (cacheControl <- response getHeader("Cache-Control"); maxAge <- MaxAge findFirstMatchIn cacheControl)
       yield maxAge group(1) toLong
   }
@@ -42,7 +34,5 @@ class CacheEntry(response: ResponseWrapper) {
     )
   }
 
-  def isFresh = {
-    freshnessLifetime map (_ > currentAge) getOrElse false
-  }
+  def isFresh = freshnessLifetime map (_ > currentAge) getOrElse false
 }
