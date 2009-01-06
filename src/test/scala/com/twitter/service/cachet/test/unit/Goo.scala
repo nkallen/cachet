1a2f33048c41b4858d242031946ac58c0f705e70
Nick Kallen <nkallen@yahoo.com>
2009-01-05 17:57:42 -0800

    intellij settings

---
+++ src/test/scala/com/twitter/service/cachet/test/unit/Goo.scala
@@ -0,0 +1,11 @@
+package test.scala.com.twitter.service.cachet.test.unit
+
+import junit.framework.TestSuite
+import org.junit.Test
+
+class Goo {
+  @Test
+  def testgoo() {
+    assert(2 == 3)
+  }
+}
