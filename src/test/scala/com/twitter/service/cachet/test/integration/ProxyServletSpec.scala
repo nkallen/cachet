ebdc3977e848d39d335b67b7346020e8b5a43a1e
Nick Kallen <nkallen@yahoo.com>
2009-03-09 14:49:34 -0700

    notes for an integration tests

---
+++ src/test/scala/com/twitter/service/cachet/test/integration/ProxyServletSpec.scala
@@ -0,0 +1,21 @@
+package com.twitter.service.cachet.test.integration
+
+import org.specs.Specification
+import servlet.ProxyServlet
+
+object ProxyServletSpec extends Specification {
+  "ProxyServlet" >> {
+    "when the backend is slow" >> {
+      //      val server = new Server
+      //      server.addServlet(new SleepServlet(1))
+      //      server.addServlet(new ProxyServlet(1))
+      //      server.start()
+      //
+      //      request = new HttpTester
+      //      response = new HttpTester
+      //      request.setMethod("GET")
+      //      response.parse(tester.getResponses(request.generate))
+      //      response.getStatus mustEqual 200
+    }
+  }
+}
