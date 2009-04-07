44d0fe18a0e8d1b541a55d4c68c655cad95b7dbe
Steve Jenson <stevej@linebacker.local>
2009-04-07 15:22:19 -0700

    add configgy, use a proxy_port argument.

--- src/main/scala/com/twitter/service/cachet/Main.scala
+++ src/main/scala/com/twitter/service/cachet/Main.scala
@@ -1,10 +1,20 @@
 package com.twitter.service.cachet
 
 import limiter.LimitingProxyServletFilter
+import net.lag.configgy.{Config, Configgy, RuntimeEnvironment}
+import net.lag.logging.Logger
 
 object Main {
+  private val log = Logger.get
+  private val runtime = new RuntimeEnvironment(getClass)
+
   def main(args: Array[String]) {
-    val server = new Server(1234)
+    runtime.load(args)
+
+    val PROXY_PORT = Configgy.config.getInt("proxy_port", 1234)
+    val server = new Server(PROXY_PORT)
+    log.info("Proxy Server listening on port: %s", PORT)
+
     //server.addFilter(new LimitingProxyServletFilter, "/")
     server.addServlet(new ProxyServlet("localhost", 80, 5000), "/")
     server.start()
