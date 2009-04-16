0e1aa823930c56f7bbd2b7874f4bd3aff83f6db8
Steve Jenson <stevej@twitter.com>
2009-04-16 16:42:29 -0700

    Implemented error handling for backend failures so the tests pass with this client.

--- src/main/scala/com/twitter/service/cachet/proxy/client/ApacheHttpClient.scala
+++ src/main/scala/com/twitter/service/cachet/proxy/client/ApacheHttpClient.scala
@@ -1,11 +1,13 @@
 package com.twitter.service.cachet.proxy.client
 
+import net.lag.logging.Logger
 import javax.servlet.http.{HttpServletResponse, HttpServletRequest}
 import java.io.InputStream
-import java.net.URI
+import java.net.{ConnectException, SocketTimeoutException, URI}
 import org.apache.http.client.methods.HttpUriRequest
 import org.apache.http.conn.scheme.{SchemeRegistry, Scheme}
 import org.apache.http.entity.InputStreamEntity
+import org.apache.http.params.CoreConnectionPNames
 import org.apache.http.HttpVersion
 import org.apache.http.impl.client.{DefaultHttpClient, RequestWrapper}
 import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager
@@ -17,7 +19,10 @@ import org.apache.http.conn.scheme.PlainSocketFactory
 class ApacheHttpClient(timeout: Long, numThreads: Int) extends HttpClient {
   // FIXME: Timeout is not used
   private val params = new BasicHttpParams
+  private val log = Logger.get
   ConnManagerParams.setMaxTotalConnections(params, numThreads)
+  params.setIntParameter(CoreConnectionPNames.SO_TIMEOUT, timeout.toInt)
+  params.setIntParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, timeout.toInt)
 
   private val schemeRegistry = new SchemeRegistry
   // FIXME: Also support HTTPS
@@ -32,12 +37,33 @@ class ApacheHttpClient(timeout: Long, numThreads: Int) extends HttpClient {
     val httpHost = new org.apache.http.HttpHost(host, port, requestSpecification.scheme)
 
     // FIXME: add timings.
-    val response = client.execute(httpHost, request)
+    try {
+      val response = client.execute(httpHost, request)
+      log.info("client params: " + client.getParams())
 
-    for (header <- response.getAllHeaders)
-      servletResponse.addHeader(header.getName, header.getValue)
+      for (header <- response.getAllHeaders)
+        servletResponse.addHeader(header.getName, header.getValue)
 
-    if (response.getEntity != null) response.getEntity.writeTo(servletResponse.getOutputStream)
+      if (response.getEntity != null) response.getEntity.writeTo(servletResponse.getOutputStream)
+      Stats.w3c.log("rs-response-code", response.getStatusLine().getStatusCode())
+      if (response.getEntity() != null) {
+        Stats.w3c.log("rs-content-length", response.getEntity().getContentLength())
+        if (response.getEntity.getContentType() != null) {
+          Stats.w3c.log("rs-content-type", response.getEntity().getContentType().getValue())
+        }
+      }
+    } catch {
+      case _: SocketTimeoutException => {
+        log.warning("backend timed out while reading, returning 504 to client.")
+        Stats.w3c.log("sc-response-code", HttpServletResponse.SC_GATEWAY_TIMEOUT)
+        servletResponse.setStatus(HttpServletResponse.SC_GATEWAY_TIMEOUT)
+      }
+      case _: ConnectException => {
+        log.warning("backend timed out while connecting, returning 504 to the client.")
+        Stats.w3c.log("sc-response-code", HttpServletResponse.SC_GATEWAY_TIMEOUT)
+        servletResponse.setStatus(HttpServletResponse.SC_GATEWAY_TIMEOUT)
+      }
+    }
   }
 
   private class ApacheRequest(method: String, uri: String, headers: Seq[(String, String)], inputStream: InputStream) extends org.apache.http.client.methods.HttpEntityEnclosingRequestBase with org.apache.http.HttpRequest {
