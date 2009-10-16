2e07b3531614bd0f3159ee82e946098331886abc
Steve Jenson <stevej@twitter.com>
2009-10-16 11:07:24 -0700

    Refactor and add a test for an edge case we see in prod.

--- src/test/scala/com/twitter/service/cachet/test/integration/ProxyServletSpec.scala
+++ src/test/scala/com/twitter/service/cachet/test/integration/ProxyServletSpec.scala
@@ -10,7 +10,9 @@ import org.specs.Specification
 object ProxyServletSpec extends Specification {
   var i = 0
 
-  def makeRequestThroughProxy(sleepTime: Long, method: String): HttpTester = {
+  def makeRequestThroughProxy(request: HttpTester): HttpTester = makeRequestThroughProxy(request, 0)
+
+  def makeRequestThroughProxy(request: HttpTester, sleepTime: Long): HttpTester = {
     val proxyServer = new TestServer(2345+i, 0, 1, Nil, "data/keystore", "asdfasdf", "asdfasdf")
     val proxyProps = new Properties()
     proxyProps.put("backend-port", (3000+i).toString)
@@ -25,17 +27,21 @@ object ProxyServletSpec extends Specification {
     i += 1 // to not run into address binding issues
     slowServer.start()
 
-    val request = new HttpTester
     val response = new HttpTester
+    response.parse(proxyServer(request.generate))
+    proxyServer.stop()
+    slowServer.stop()
+    response
+  }
+
+  def makeRequestThroughProxy(sleepTime: Long, method: String): HttpTester = {
+    val request = new HttpTester
     request.addHeader("X-Forwarded-For", "1.1.1.1")
     request.setMethod(method)
     request.setURI("/")
     request.setVersion("HTTP/1.0")
-    response.parse(proxyServer(request.generate))
 
-    proxyServer.stop()
-    slowServer.stop()
-    response
+    makeRequestThroughProxy(request, sleepTime)
   }
 
   // By default, do a GET request
@@ -70,6 +76,22 @@ object ProxyServletSpec extends Specification {
         val response = makeRequestThroughProxy(0, "HEAD")
         response.getStatus mustEqual HttpServletResponse.SC_OK
       }
+    }
+
+    "send a request with a Content-Type and a Content-Length of 0" >> {
+      val request = new HttpTester()
+      request.setMethod("POST")
+      request.setURI("/")
+      request.setVersion("HTTP/1.0")
+      request.addHeader("X-Forwarded-For", "1.1.1.1")
+      request.addHeader("Content-Length", "0")
+      request.addHeader("Host", "twitter.com")
+      request.addHeader("Accept", "*/*")
+      request.addHeader("Authorization", "Basic BIGTIMEFOOBARXAXOqluavTYbmV0Mz==")
+
+      val response = makeRequestThroughProxy(request)
+
+      response.getStatus mustEqual HttpServletResponse.SC_OK
     }
   }
 }
